7a41d3590b929fc18de123e8df2d2b9a79f7c4d0
Akshay Tyagi <35887621+akshay20t@users.noreply.github.com>
2019-10-03 23:34:36 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -8,9 +8,7 @@ class User(auth.models.User,auth.models.PermissionsMixin):
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User)
-
-    gender = models.CharField(max_length=8)
-    profile_pic = models.ImageField(upload_to="profile_pics",blank=True)
+    user1= models.OneToOneField(User)
 
     def __str__(self):
         return "@{}".format(self.user.username)
